72921c12d3874a3321e1530f48bf9a72df69adc7
Nikolay Kozyrskiy <nik.kozyrskiy@gmail.com>
2017-11-22 01:26:41 +0300

    refactoring

--- upow.py
+++ upow.py
@@ -1,18 +1,9 @@
 import seaborn as sns
 from numpy.linalg import solve
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 import pylab
 import random
-
-import scipy as stats
-import scipy.special as special
-from scipy.integrate import quad
-import pandas as pd
-import pprint
-import csv
-import scipy.special
 
 from sympy import Matrix
 import hashlib
@@ -21,8 +12,9 @@ sns.set()
 
 
 def Gen(x, p, k, d):
-    gen = np.zeros((k * d + 1) * len(x))
-    r = np.zeros(len(x) * (k * d + 1))
+    length = (k * d + 1) * len(x)
+    gen = np.zeros(length)
+    r = np.zeros(length)
     for i in range(len(x)):
         r[i] = random.randint(0, p)
     for t in range(1, k * d + 2):
@@ -30,42 +22,68 @@ def Gen(x, p, k, d):
             gen[(t-1) * len(x) + i] = t * r[i]
     return gen
 
+def Solve(y, n, k, d, p):
+    gOV = np.zeros(k * d + 1)
+    q_s_pol_coeffs_quantity = (n - 1) * d + 1
+    alpha = []
+    tau = []
+    x = np.zeros(q_s_pol_coeffs_quantity)
 
-# todo: implement
+    for t in range(1, k * d + 2):
+        y_t = y[(t-1) * n * k * d : t * n * k * d]
+        q_1_s = get_q_s(y_t, n, k, d, p, 0, [])
+        for i in range(n):
+            for j in range(q_s_pol_coeffs_quantity):
+                x[j] = (i ** (q_s_pol_coeffs_quantity-j-1)) % p
+            gOV[t-1] = (gOV[t-1] + sum(q_1_s * x)) % p
+        tau.append(gOV[t-1])
+        tau.append(q_1_s)
+        print("gOV in Solve = ", gOV)
+        for s in range(1, k - 1):
+            # todo check hash
+            alpha.append(int(hashlib.sha256(np.array([1, 1, 1])).hexdigest(), 16) % n)  # hash
+            print(alpha, "solve")
+            # print(alpha, "alpha")
+            tau.append(get_q_s(y_t, n, k, d, p, s, alpha))
+    print("tau[1] = ", tau[1])
+    return tau
+
+
 def Verify(y, n, k, d, p, tau):
-    alpha=[]
+    accept = "accept"
+    reject = "reject"
+    alpha = []
     q_s_pol_coeffs_quantity = (n - 1) * d + 1
     x = np.zeros(q_s_pol_coeffs_quantity)
-    gOV=0
-    for t in range(1, k*d + 2):
-        gOV=0
-        y_t = y[(t-1) * n * k * d: (t) * n * k * d]
+    gOV = 0
+    for t in range(1, k * d + 2):
+        gOV = 0
+        y_t = y[(t-1) * n * k * d: t * n * k * d]
         for i in range (n):
             for j in range(q_s_pol_coeffs_quantity):
-                x[j] = (i ** (q_s_pol_coeffs_quantity - j-1)) % p
-            gOV=gOV+sum(tau[t*k+1-k]*x)
+                x[j] = (i ** (q_s_pol_coeffs_quantity - j - 1)) % p
+            gOV = gOV + sum(tau[t * k + 1 - k] * x)
         print("eeeeeeeeeeeeeeeeee")
-        if (gOV%p!=tau[t*k-k]):
+        if (gOV % p != tau[t * k - k]):
             print("ededede")
             return "1reject"
 
-        print("k-2 = ", k-2)
         for s in range(0, k-2):
             gOV = 0
             # todo: check
             print("blyaaaaaaaa")
-            alpha.append(int(hashlib.sha256(1).hexdigest(), 16) % n)# todo check
+            alpha.append(int(hashlib.sha256(1).hexdigest(), 16) % n)  # todo check
             for j in range(q_s_pol_coeffs_quantity):
                 x[j] = (alpha[s] ** (q_s_pol_coeffs_quantity - j - 1)) % p
-            q_alpha_s=sum(tau[t*k+s+1-k]*x)
+            q_alpha_s=sum(tau[t * k + s+1 - k] * x)
             for i in range(n):
                 for j in range(q_s_pol_coeffs_quantity):
                     x[j] = (i ** (q_s_pol_coeffs_quantity - j-1)) % p
-                gOV = gOV + sum(tau[t * k + s + 2 - k  ] * x)
-            if (q_alpha_s!=gOV):
+                gOV = gOV + sum(tau[t * k + s + 2 - k ] * x)
+            if (q_alpha_s != gOV):
                 return "2reject"
                 # todo: check
-        gOV=0
+        gOV = 0
         alpha.append(int(hashlib.sha256(np.array([1, 1, 1])).hexdigest(), 16) % n)
         print("alpha = ", alpha)
         coeffs_real=np.zeros((n-1) * d + 1)
@@ -75,7 +93,7 @@ def Verify(y, n, k, d, p, tau):
                 x[j] = (i ** (q_s_pol_coeffs_quantity - j-1)) % p
             gOV=gOV+sum(q_real_coeff*x)
         for j in range(q_s_pol_coeffs_quantity):
-            x[j] = (alpha[len(alpha)-1] ** (q_s_pol_coeffs_quantity - j-1)) % p
+            x[j] = (alpha[len(alpha) - 1] ** (q_s_pol_coeffs_quantity - j-1)) % p
         q_sum_solver=sum(tau[t * k - 1] * x)
         print("q_sum_solver % p = ", q_sum_solver % p)
         print("gOV % p = ", gOV % p)
@@ -83,11 +101,7 @@ def Verify(y, n, k, d, p, tau):
         gOV = gOV % p
         if (q_sum_solver != gOV):
             return "3reject"
-    return "accept"
-
-
-
-
+    return accept
 
 
 def make_x_vector(n, k, d):
@@ -104,7 +118,7 @@ def get_phi_s_l_polinom(y, n, k, d, p, s, l):
         for j in range(n):
             Matrix_p[i][j] = (i ** j) % p
     #coeffs - коэффициенты выходного полинома phi_s_l
-    coeffs = Matrix(Matrix_p).inv_mod(p) * Matrix(y[s * n * d + l * n : s * n * d + (l + 1) * n])
+    coeffs = Matrix(Matrix_p).inv_mod(p) * Matrix(y[s * n * d + l * n: s * n * d + (l + 1) * n])
     for i in range(len(coeffs)):
         result_coeffs[i] = coeffs[i] % p
     return result_coeffs
@@ -140,33 +154,6 @@ def get_q_s(y, n, k, d, p, s, alpha):
         coeffs_res = coeffs_res + coeffs
 
     return coeffs_res % p
-
-
-def Solve(y, n, k, d, p):
-    gOV = np.zeros(k * d+1)
-    q_s_pol_coeffs_quantity = (n - 1) * d + 1
-    alpha = []
-    tau = []
-    # todo: + 1  ????
-    x = np.zeros(q_s_pol_coeffs_quantity)
-
-    for t in range(1, k*d+2):
-        y_t = y[(t-1) * n * k * d : (t) * n * k * d]
-        q_1_s = get_q_s(y_t, n, k, d, p, 0, [])
-        for i in range(n):
-            for j in range(q_s_pol_coeffs_quantity):
-                x[j] = (i **(q_s_pol_coeffs_quantity-j-1)) % p
-            gOV[t-1] = (gOV[t-1] + sum(q_1_s * x)) % p
-        tau.append(gOV[t-1])
-        tau.append(q_1_s)
-        for s in range(1, k - 1):
-            # todo:  check
-            alpha.append(int(hashlib.sha256(np.array([1,1,1])).hexdigest(), 16) % n)  #### hash
-            print(alpha, "solve")
-            # print(alpha, "alpha")
-            tau.append(get_q_s(y_t, n, k, d, p, s, alpha))
-    print("tau[1] = ", tau[1])
-    return tau
 
 
 n = 3
